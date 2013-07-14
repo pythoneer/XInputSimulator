8905757ae5553fa0ece0e2bd04f95bfe91a2cb22
Dustin Bensing <dustin.bensing@googlemail.com>
2013-07-14 14:25:29 +0200

    added LGPL

--- XInputSimulator/xinputsimulator.cpp
+++ XInputSimulator/xinputsimulator.cpp
@@ -1,3 +1,20 @@
+//    Copyright 2013 Dustin Bensing
+
+//    This file is part of XInputSimulator.
+
+//    XInputSimulator is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    XInputSimulator is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser Public License for more details.
+
+//    You should have received a copy of the GNU Lesser Public License
+//    along with XInputSimulator.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "xinputsimulator.h"
 
 void XInputSimulator::mouseMoveTo(int x, int y)
